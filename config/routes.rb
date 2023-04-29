219a0712b7d2733d5ebd45fd4c053cc7a5a9d1ea
Mayara Vass <mayara_mv93@outlook.com>
2023-04-29 17:19:48 -0300

    Watch List

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # root to: "menu#index"
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: :destroy
 end
